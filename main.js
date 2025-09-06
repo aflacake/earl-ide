17ac44442ea3459eba72f3a39cbd140df1e777d1
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-09-06 07:49:16 +0700

    Hubungkan dengan folder `earl-lang`
    Di `main.js` tambahkan _handler_ IPC.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const { app, BrowserWindow, ipcMain } = require("electron");
 const jalur = require("path");
+const { runEarl } = require("../earl-lang/pemroses.js");
 
 function membuatJendela() {
   const win = new BrowserWindow({
@@ -19,4 +20,20 @@ app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.handle("run-earl", async (event, code) => {
+  let buffer = [];
+  const originalLog = console.log;
+
+  console.log = (...args) => buffer.push(args.join(" "));
+
+  try {
+    await runEarl(code);
+  } catch (err) {
+    buffer.push("Kesalahan: " + err.message);
+  }
+
+  console.log = originalLog;
+  return buffer.join("\n");
 });
